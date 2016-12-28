67f9e1dbe01c1a51a0bee0cf5fafec74fe8846ee
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2016-12-28 02:33:38 +0100

    Fixed the possibly unhandled rejection bug by removing the callback

--- services.js
+++ services.js
@@ -6,7 +6,7 @@ myWeatherForecastApp.service('cityService', function () {
 
 myWeatherForecastApp.service('weatherService', ['$resource', function ($resource) {
     this.getWeatherInfo = function(city, days) {
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {get: {method: "JSONP"}});
         return weatherAPI.get({ q: city, cnt: days, APPID: '7cc97faceed75bd9207b0632b15de297'});
     };
 }]);
